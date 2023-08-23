a9ae3f2d84f6bdcaa8eeb7ec33f1214e93c82a54
Mixarder <tar.mixard@yandex.ru>
2023-08-24 01:26:27 +0300

    add_script_headers

--- try_exept.py
+++ try_exept.py
@@ -1,11 +1,12 @@
-from json.decoder import JSONDecodeError
+from json.decoder import JSONDecodeError  # Импорт ошибки, который мы будем ожидать
 import requests
 
 response = requests.get("https://playground.learnqa.ru/api/get_text")
 print(response.text)
 
+
 try:
-    parsed_response_text = response.json()
-    print(parsed_response_text)
+    parsed_response_text = response.json()     # Код с парсингом текста ответа и его выводом, поместили в блок try
+    print(parsed_response_text)                # это означает, что мы попробуем его выполнить
 except JSONDecodeError:
     print("Response is not a JSON format")
