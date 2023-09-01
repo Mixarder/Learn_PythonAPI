5cd704fb9c14cf6db118061f056a7890c45880b5
Mixarder <tar.mixard@yandex.ru>
2023-09-01 23:51:08 +0300

    update_parametrize

--- test_parametrize_api.py
+++ test_parametrize_api.py
@@ -9,6 +9,20 @@ class TestParametrizeApi:
         url = "https://playground.learnqa.ru/api/hello"
         data = {'name':name}
 
+
         response = requests.get(url,params=data)
 
         assert response.status_code == 200, "Wrong response"
+
+
+        response_dict = response.json()
+
+        assert "answer" in response_dict, "отсутствует поле 'answer' в ответе"
+        if len(name) == 0:
+            expected_response_text = "Hello, someone"
+        else:
+
+            expected_response_text = f"Hello, {name}"
+
+        actual_response_text = response_dict["answer"]
+        assert actual_response_text == expected_response_text, "Актуальный текст в ответе не корректен"
